d8c7556af38bf94398b6460ffb3ffff474779264
TSMotter <mottergiuliano@gmail.com>
2023-02-05 09:43:18 -0300

    Add image of study about big O notation;

--- create.sh
+++ create.sh
@@ -5,7 +5,7 @@ mkdir $1
 echo "# Creates a binary target
 add_executable($1 $1.cpp)" > $1/CMakeLists.txt
 
-cp template/template.cpp $1/$1.cpp
+cp helloworld/helloworld.cpp $1/$1.cpp
 
 touch $1/readme.md
 
